ee552e201624621f2d789bc1d3c853012eff32b2
Jocke Wilen <jochum.wilen@gmail.com>
2021-06-22 08:50:19 +0300

    bmi from home

--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
+    
+    let quiz = [
+        ["Four plus Two equal to Six", "True"],
+        ["Ten minus Two is smaller than Five.", "False"],
+        ["Forty-Two diveded with Zero is One.", "True"]
+    ]
+    
+    var questionNbr = 0
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
+        
     }
 
-
+    @IBAction func anwserBtnPressed(_ sender: UIButton) {
+        let userAnwser = sender.currentTitle
+        let actualAnwser = quiz[questionNbr][1]
+       
+        if userAnwser == actualAnwser {
+            print("Right!")
+        }
+        else {
+            print("Wrong!")
+        }
+        if questionNbr + 1 < quiz.count {
+        questionNbr += 1
+        }
+        else {
+            questionNbr = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        
+        questionLabel.text = quiz[questionNbr][0]
+    }
+    
 }
 
