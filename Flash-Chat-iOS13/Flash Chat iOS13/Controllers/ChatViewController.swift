fe630a1c908344165e95dc0e499b9adaae7ba81e
jwilen <jochum.wilen@gmail.com>
2021-07-11 14:24:48 +0300

    working on app in school

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
 
@@ -15,6 +16,8 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = K.appName
+        navigationItem.hidesBackButton = true
 
     }
     
@@ -22,4 +25,17 @@ class ChatViewController: UIViewController {
     }
     
 
+    @IBAction func logOut(_ sender: UIBarButtonItem) {
+        
+        do {
+          try  Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+            
+            
+            
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+          
+    }
 }
--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -15,6 +16,21 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
-    }
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let e = error
+            {
+                let alert = UIAlertController(title: "Alert", message: e.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                     alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                     
+                 }
+                 else
+                 {
+                    self.performSegue(withIdentifier: K.loginSegue, sender: self)
+                 }
+                 }
+             }
+        }
     
 }
