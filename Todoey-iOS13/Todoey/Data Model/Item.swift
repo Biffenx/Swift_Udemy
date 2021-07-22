9004e87d97d74ad104eede14b15a335fe8901e3d
jwilen <jochum.wilen@gmail.com>
2021-07-22 16:27:01 +0300

    doing some updates in school

---
+++ Todoey-iOS13/Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jochum Wilen on 7/22/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
