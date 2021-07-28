0f3101910909cc205ad28c5931f3b253bd6f6996
jwilen <jochum.wilen@gmail.com>
2021-07-28 12:48:25 +0300

    looking for bug...

--- Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
+++ Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
@@ -12,18 +12,21 @@ import CoreData
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
+    
+    var selectedCategory : Category? {
+        didSet{
+            loadItems()
+        }
+    }
 //    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        print(dataFilePath)
 
-        loadItems()
     }
     
-    //MARK - Tableview Datasources Methods
+    //MARK: - Tableview Datasources Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -38,14 +41,12 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
-    // MARK - TavleView Delegate Methods
+    // MARK: - TavleView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
         saveItems()
-   
         tableView.deselectRow(at: indexPath, animated: true)
         }
 
@@ -60,6 +61,7 @@ class TodoListViewController: UITableViewController {
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             
             self.itemArray.append(newItem)
             
@@ -74,7 +76,7 @@ class TodoListViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    //MARK - Model Manupulation Method
+    //MARK: - Model Manupulation Method
     
     func saveItems() {
         
@@ -86,16 +88,43 @@ class TodoListViewController: UITableViewController {
                self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        if let addtionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, addtionalPredicate])
+        }else{
+            request.predicate = categoryPredicate
+        }
+
         do {
         itemArray = try context.fetch(request)
         }catch{
             print("Error fetching data fom context \(error)")
         }
+        tableView.reloadData()
     }
-    
-    
 }
 
+//MARK: - Search bar methods
 
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        loadItems(with: request, predicate: predicate)
+      }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+    }
+}
