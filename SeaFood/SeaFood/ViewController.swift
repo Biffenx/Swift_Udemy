6502178dbc71084998f1b53f0f018e05bda018d1
jwilen <jochum.wilen@gmail.com>
2021-08-15 10:40:05 +0300

    Seafood app start

---
+++ SeaFood/SeaFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Jochum Wilen on 8/15/21.
+//  Copyright © 2021 Jochum Wilen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
